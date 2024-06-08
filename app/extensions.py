0d04eb20bf44d1d30072872d216c88e0a68d68c7
Robert Sneiderman <48200016+Robby955@users.noreply.github.com>
2024-06-08 12:51:57 -0500

    Delete app directory

--- app/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-
-# Create the SQLAlchemy instance
-db = SQLAlchemy()
-
-# Create the LoginManager instance
-login_manager = LoginManager()
